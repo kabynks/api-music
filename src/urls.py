bc7d5d27a0ebcf093245607d5d8399156570b953
kabynks <kabynks@gmail.com>
2024-03-06 04:26:49 -0800

    release

--- src/urls.py
+++ src/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("rest_framework.urls")),
+    path("api/dj-rest-auth/", include('dj_rest_auth.urls')),
     path("api/model/", include("apps.album.urls")),
     path("api/model/", include("apps.artist.urls")),
     path("api/model/", include("apps.music.urls")),
