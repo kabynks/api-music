4a7886ede08c7e85b85d4f388dbb1ad9658c680f
kabynks <kabynks@gmail.com>
2024-03-06 05:24:32 -0800

    release

--- src/wsgi.py
+++ src/wsgi.py
@@ -13,4 +13,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings.production')
 
 application = get_wsgi_application()
+app = application
 
