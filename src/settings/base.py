85d3099f76f19014663affd2811d2c4ea8eb18cc
kabynks <kabynks@gmail.com>
2024-03-06 09:11:55 -0800

    release

--- src/settings/base.py
+++ src/settings/base.py
@@ -71,9 +71,7 @@ TEMPLATES = [
     },
 ]
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
+    
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication"
     ]
