97d2a086914a7f81a15fd77af5505223b2a55950
kabynks <kabynks@gmail.com>
2024-03-13 04:39:54 -0700

    change settings logs

--- apps/music/views.py
+++ apps/music/views.py
@@ -6,6 +6,6 @@ from .serializers import MusicSerializer
 logger = logging.getLogger("main")
 
 class MusicViewSet(ModelViewSet):
-    logger.info("music")
+    logger.warning("music")
     queryset=Music.objects.all()
     serializer_class=MusicSerializer
